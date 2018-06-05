e7a4680e037bbf7bd5e4c182c3b6da3523056c5e
fabioalvaro <fabioalvaro@fabiothundercat.assertiva>
2018-06-05 13:21:15 -0300

    Novo exercicio

--- src/main/java/br/com/alura/ValidacaoDocumento.java
+++ src/main/java/br/com/alura/ValidacaoDocumento.java
@@ -1,45 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.alura;
 
+import br.com.caelum.stella.validation.CNPJValidator;
 import br.com.caelum.stella.validation.CPFValidator;
 import br.com.caelum.stella.validation.InvalidStateException;
 import br.com.caelum.stella.validation.TituloEleitoralValidator;
+import br.com.caelum.stella.validation.Validator;
 
-/**
- *
- * @author fabioalvaro
- */
 public class ValidacaoDocumento {
+	
+	public static void main(String[] args) {
+		String cpf = "862.883.667-57";
+		try{
+			validarDocumentos(new CPFValidator(), cpf);
+			System.out.println("CPF VÁLIDO");
+			
+		}catch (InvalidStateException e) {
+			System.out.println("CPF INVÁLIDO : " + e);
+		}
+		String cnpj = "18022756000105";
+		try{
+			validarDocumentos(new CNPJValidator(), cnpj);
+			System.out.println("CNPJ VALIDO");
+			
+		}catch (InvalidStateException e) {
+			System.out.println("CNPJ INVALIDO : " + e);
+		}
+		String tituloEleitor = "417453530116";
+		try{
+			validarDocumentos(new TituloEleitoralValidator(), tituloEleitor);
+			System.out.println("TITULO VALIDO");
+			
+		}catch (InvalidStateException e) {
+			System.out.println("TITULO INVALIDO : " + e);
+		}
+		
+		
+	}
+	
+	private static void validarDocumentos(Validator<String> validador, String documento){
+		validador.assertValid(documento);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
-    public static void main(String[] args) {
-
-        //String cpf = "26721993880";
-        String cpf = "26721993899";
-        String tituloEleitor = "757888710124";
-        
-        CPFValidator validador = new CPFValidator();
-        try {
-            validador.assertValid(cpf);
-             System.out.println("CPF Válido: ");
-        } catch (InvalidStateException e) {
-            System.out.println("CPF Invalido: " + e);
-        }
-        
-        //Titulo
-        TituloEleitoralValidator validadorTitulo = new TituloEleitoralValidator();
-        try {
-            validadorTitulo.assertValid(tituloEleitor);
-             System.out.println("Titulo Eleitor Válido: ");
-        } catch (InvalidStateException e) {
-            System.out.println("Titulo Invalido: " + e);
-        }
-        
-        
-
-        System.out.println("Entrou");
-    }
 }
